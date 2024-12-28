98e6457283f6e8615e01b34de0f30e66a8aabf90
David Alsabrook <Dfalsabrook@gmail.com>
2024-12-28 23:47:47 +0000

    Little style update for signup

--- app/components/header.js
+++ app/components/header.js
@@ -194,7 +194,7 @@ export default function Header() {
               <DialogTrigger asChild>
                 <Button variant="default">Sign up</Button>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
+                <DialogContent className="sm:max-w-[725px]">
                 <DialogHeader>
                   <DialogTitle>Sign Up</DialogTitle>
                   <DialogDescription>
@@ -202,100 +202,104 @@ export default function Header() {
                   </DialogDescription>
                 </DialogHeader>
                   <form onSubmit={handleSignUp}>
-                    <div className="grid gap-4 py-4">
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="firstName">
-                          First Name
-                        </Label>
-                        <Input
-                          id="firstName"
-                          value={firstName}
-                          onChange={(e) => setFirstName(e.target.value)}
-                          className="col-span-3"
-                        />
+                    <div className="grid grid-cols-2 gap-4 py-4">
+                      <div className="grid grid-cols-1 gap-4" style={{ minWidth: '200px', maxWidth: '300px' }}>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="firstName" className="text-lg">
+                            First Name
+                          </Label>
+                          <Input
+                            id="firstName"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="lastName" className="text-lg">
+                            Last Name
+                          </Label>
+                          <Input
+                            id="lastName"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="userRole" className="text-lg">
+                            I am a
+                          </Label>
+                          <select
+                            id="userRole"
+                            value={userRole}
+                            onChange={(e) => setUserRole(e.target.value)}
+                            className="col-span-3 border"
+                          >
+                            <option value="">Select your role</option>
+                            <option value="homeowner">Homeowner (Personal home)</option>
+                            <option value="landlord">Property Owner (Landlord)</option>
+                            <option value="tenant">Tenant</option>
+                            <option value="designer">Designer</option>
+                            <option value="contractor">Contractor</option>
+                            <option value="other">Other</option>
+                          </select>
+                        </div>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="referralSource" className="text-lg">
+                            How did you hear about us?
+                          </Label>
+                          <select
+                            id="referralSource"
+                            value={referralSource}
+                            onChange={(e) => setReferralSource(e.target.value)}
+                            className="col-span-3 border"
+                          >
+                            <option value="">Select an option</option>
+                            <option value="search_engine">Search Engine</option>
+                            <option value="advertisement">Advertisement</option>
+                            <option value="friend">Friend</option>
+                            <option value="other">Other</option>
+                          </select>
+                        </div>
                       </div>
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="lastName">
-                          Last Name
-                        </Label>
-                        <Input
-                          id="lastName"
-                          value={lastName}
-                          onChange={(e) => setLastName(e.target.value)}
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="userRole">
-                          I am a
-                        </Label>
-                        <select
-                          id="userRole"
-                          value={userRole}
-                          onChange={(e) => setUserRole(e.target.value)}
-                          className="col-span-3"
-                        >
-                          <option value="">Select your role</option>
-                          <option value="homeowner">Homeowner (Personal home)</option>
-                          <option value="landlord">Property Owner (Landlord)</option>
-                          <option value="tenant">Tenant</option>
-                          <option value="designer">Designer</option>
-                          <option value="contractor">Contractor</option>
-                          <option value="other">Other</option>
-                        </select>
-                      </div>
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="referralSource">
-                          How did you hear about us?
-                        </Label>
-                        <select
-                          id="referralSource"
-                          value={referralSource}
-                          onChange={(e) => setReferralSource(e.target.value)}
-                          className="col-span-3"
-                        >
-                          <option value="">Select an option</option>
-                          <option value="search_engine">Search Engine</option>
-                          <option value="advertisement">Advertisement</option>
-                          <option value="friend">Friend</option>
-                          <option value="other">Other</option>
-                        </select>
-                      </div>
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="signUpEmail">
-                          Email
-                        </Label>
-                        <Input
-                          id="signUpEmail"
-                          type="email"
-                          value={signUpEmail}
-                          onChange={(e) => setSignUpEmail(e.target.value)}
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="signUpPassword">
-                          Password
-                        </Label>
-                        <Input
-                          id="signUpPassword"
-                          type="password"
-                          value={signUpPassword}
-                          onChange={(e) => setSignUpPassword(e.target.value)}
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="grid grid-cols-1 items-center gap-4">
-                        <Label htmlFor="confirmPassword">
-                          Confirm Password
-                        </Label>
-                        <Input
-                          id="confirmPassword"
-                          type="password"
-                          value={confirmPassword}
-                          onChange={(e) => setConfirmPassword(e.target.value)}
-                          className="col-span-3"
-                        />
+                      <div className="grid grid-cols-1 gap-4" style={{ minWidth: '200px', maxWidth: '300px' }}>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="signUpEmail" className="text-lg">
+                            Email
+                          </Label>
+                          <Input
+                            id="signUpEmail"
+                            type="email"
+                            value={signUpEmail}
+                            onChange={(e) => setSignUpEmail(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="signUpPassword" className="text-lg">
+                            Password
+                          </Label>
+                          <Input
+                            id="signUpPassword"
+                            type="password"
+                            value={signUpPassword}
+                            onChange={(e) => setSignUpPassword(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
+                        <div className="grid grid-cols-1 items-center gap-4">
+                          <Label htmlFor="confirmPassword" className="text-lg">
+                            Confirm Password
+                          </Label>
+                          <Input
+                            id="confirmPassword"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
                       </div>
                     </div>
                     {signUpError && <p className="text-red-500 text-sm mt-2">{signUpError}</p>}
