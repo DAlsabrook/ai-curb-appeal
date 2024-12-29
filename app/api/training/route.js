3fa50f014e498f2787fd63c7413dc6431f742b93
David Alsabrook <Dfalsabrook@gmail.com>
2024-12-29 21:09:41 +0000

    Working locally but lets try it in production.

--- app/api/training/route.js
+++ app/api/training/route.js
@@ -74,128 +74,172 @@ async function checkEnvVariables() {
 
 // Function to handle the POST request
 export async function POST(req) {
+  Logger.info('Training Route - POST request received.');
 
   if (!checkEnvVariables()) {
     Logger.error('Training Route - Did not pass all env var checks.');
     return NextResponse.json({ detail: "Environment variables not set correctly in training route" }, { status: 400 });
   }
 
-  const formData = await req.formData();
-  const userGivenName = formData.get('name').toLowerCase().replace(/ /g, '_');
-  const userUID = formData.get('uid');
+  try {
+    const formData = await req.formData();
+    const userGivenName = formData.get('name').toLowerCase().replace(/ /g, '_');
+    const userUID = formData.get('uid');
 
-  if (!userGivenName) {
-    Logger.error('Training Route - Model name is required.');
-    return NextResponse.json({ detail: "Model name is required" }, { status: 400 });
-  }
-
-  // Check if the name contains only letters and numbers
-  const isValidName = /^[a-zA-Z0-9_]+$/.test(userGivenName);
-  if (!isValidName) {
-    Logger.error('Training Route - Model name must contain only letters and numbers');
-    return NextResponse.json({ detail: "Model name must contain only letters and numbers" }, { status: 400 });
-  }
-
-  // Collect all images from formData
-  const images = [];
-  formData.forEach((value, key) => {
-    if (key.startsWith('filesList[')) {
-      images.push(value);
+    if (!userGivenName) {
+      Logger.error('Training Route - Model name is required.');
+      return NextResponse.json({ detail: "Model name is required" }, { status: 400 });
     }
-  });
 
-  if (images.length === 0) {
-    Logger.error('Training Route - Did not pass images > 0 check.');
-    return NextResponse.json({ detail: "Error loading images" }, { status: 400 });
-  }
+    // Check if the name contains only letters and numbers
+    const isValidName = /^[a-zA-Z0-9_]+$/.test(userGivenName);
+    if (!isValidName) {
+      Logger.error('Training Route - Model name must contain only letters and numbers');
+      return NextResponse.json({ detail: "Model name must contain only letters and numbers" }, { status: 400 });
+    }
 
-  // Resize images to a maximum size of 1MB
-  const resizedImages = await resizeImages(images);
-
-  // Create zip file from resized images
-  const zipContent = await createZip(resizedImages);
-
-  // Need to auto caption and include cations in zip
-  // https://replicate.com/fofr/batch-image-captioning/api
-  Logger.info('Caption started')
-  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-  const input = {
-    system_prompt: "Write a six-sentence caption for this image. In the first sentence, describe the style and type of the image, emphasizing its photorealistic, high-definition quality. In the remaining sentences, provide a precise and detailed description of the home and its surroundings, focusing on architectural features, textures, and landscaping elements. Use clear, descriptive language suitable for prompting a text-to-image model, highlighting the realism and intricacy of the details. Avoid subjective phrases like 'evokes a sense of' and instead describe the exact composition and features. Comma-separate keywords rather than using 'or'. Precision and attention to architectural and environmental detail are essential.\n\nGood examples are:\n\n'Photorealistic HD photo of a modern suburban home with a white facade, large glass windows, and a dark gray roof, surrounded by a manicured lawn and a cobblestone pathway, detailed textures on the siding, realistic reflections in the windows, vibrant lighting, and a clear blue sky.'\n\n'Photorealistic high-definition image of a classic Victorian-style home with red brick walls, a steeply pitched roof, and ornate wooden trim, lush green landscaping with blooming flowers, a wraparound porch with wooden railings, and dramatic shadows cast by afternoon sunlight.'\n\n'Photorealistic HD photo of a Mediterranean-style villa with stucco walls, a terracotta roof, and arched windows, surrounded by palm trees and a gravel driveway, intricate detailing in the balcony railings, warm evening light, and a vivid orange and pink sunset sky in the background.'\n",
-    openai_api_key: OPENAI_API_KEY,
-    image_zip_archive: zipContent,
-    model: "gpt-4o-mini"
-  };
-
-  const output = await replicate.run("fofr/batch-image-captioning:d0adb15f4826881a68f1d82e0b10fe2ee1af536632dc8313f7f777ed8d264726", { input });
-
-  // Fetch the caption zip file
-  const captionZipBuffer = Buffer.from(await fetch(output).then(res => res.arrayBuffer()));
-  const captionFiles = await extractZip(captionZipBuffer);
-  // Combine resized images and caption files into a new zip file
-  const combinedFiles = [...resizedImages, ...captionFiles];
-  const combinedZipContent = await createZip(combinedFiles);
-  // Upload the combined zip file to Firebase Storage
-  const combinedDownloadURL = await uploadZip(images[0], combinedZipContent, userUID, userGivenName);
-
-  try { // Create the model
-    const owner = 'dalsabrook';
-    const visibility = 'private';
-    const hardware = 'gpu-t4';
-    const description = 'AICurbAppeal.com house model'
-
-    await replicate.models.create(
-      owner,
-      userGivenName,
-      {
-        'visibility': visibility,
-        'hardware': hardware,
-        'description': description
+    // Collect all images from formData
+    const images = [];
+    formData.forEach((value, key) => {
+      if (key.startsWith('filesList[')) {
+        images.push(value);
       }
-    );
+    });
 
-    try { // Training the model that was just created
-      const modelOwner = 'ostris';
-      const modelName = 'flux-dev-lora-trainer';
-      const versionId = '3f39d8b7d50801daf27c7adc4e6b3de138cce9c5daa22e062c5ca30f0a558918';
+    if (images.length === 0) {
+      Logger.error('Training Route - Did not pass images > 0 check.');
+      return NextResponse.json({ detail: "Error loading images" }, { status: 400 });
+    }
 
-      const options = {
-        destination: `${owner}/${userGivenName}`,
-        input: {
-          steps: 1000,
-          lora_rank: 16,
-          optimizer: "adamw8bit",
-          batch_size: 1,
-          resolution: "512,768,1024",
-          autocaption: false,
-          input_images: combinedDownloadURL.zipDownloadURL,
-          trigger_word: "TOK", // Possibly Set to House?
-          learning_rate: 0.0004,
-          wandb_project: "flux_train_replicate",
-          wandb_save_interval: 100,
-          caption_dropout_rate: 0.05,
-          wandb_sample_interval: 100
-        },
-        webhook: `https://ai-curb-appeal.vercel.app/api/training-webhook?uid=${userUID}&modelName=${userGivenName}&trainedImg=${combinedDownloadURL.imageDownloadURL}`
-        // Add query params like user.uid, model name? to then save in db from webhook?
-      };
+    try {
+      // Resize images to a maximum size of 1MB
+      Logger.info('Training Route - Resizing images.');
+      const resizedImages = await resizeImages(images);
 
-      const training = await replicate.trainings.create(modelOwner, modelName, versionId, options);
+      try {
+        // Create zip file from resized images
+        Logger.info('Training Route - Creating zip file from resized images.');
+        const zipContent = await createZip(resizedImages);
 
-      // Possibly delete the .zip from firebase after use.
-      // Not sure if it is cheaper to keep the files or use operations to delete them
+        try {
+          // Need to auto caption and include captions in zip
+          Logger.info('Training Route - Starting image captioning.');
+          const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+          const input = {
+            system_prompt: "Write a six-sentence caption for this image. In the first sentence, describe the style and type of the image, emphasizing its photorealistic, high-definition quality. In the remaining sentences, provide a precise and detailed description of the home and its surroundings, focusing on architectural features, textures, and landscaping elements. Use clear, descriptive language suitable for prompting a text-to-image model, highlighting the realism and intricacy of the details. Avoid subjective phrases like 'evokes a sense of' and instead describe the exact composition and features. Comma-separate keywords rather than using 'or'. Precision and attention to architectural and environmental detail are essential.\n\nGood examples are:\n\n'Photorealistic HD photo of a modern suburban home with a white facade, large glass windows, and a dark gray roof, surrounded by a manicured lawn and a cobblestone pathway, detailed textures on the siding, realistic reflections in the windows, vibrant lighting, and a clear blue sky.'\n\n'Photorealistic high-definition image of a classic Victorian-style home with red brick walls, a steeply pitched roof, and ornate wooden trim, lush green landscaping with blooming flowers, a wraparound porch with wooden railings, and dramatic shadows cast by afternoon sunlight.'\n\n'Photorealistic HD photo of a Mediterranean-style villa with stucco walls, a terracotta roof, and arched windows, surrounded by palm trees and a gravel driveway, intricate detailing in the balcony railings, warm evening light, and a vivid orange and pink sunset sky in the background.'\n",
+            openai_api_key: OPENAI_API_KEY,
+            image_zip_archive: zipContent,
+            model: "gpt-4o-mini"
+          };
 
-      Logger.info(`Training Route - Training URL: https://replicate.com/p/${training.id}`);
-      return NextResponse.json({ detail: 'Model training has started!', trainedModel: training }, { status: 200 });
+          const output = await replicate.run("fofr/batch-image-captioning:d0adb15f4826881a68f1d82e0b10fe2ee1af536632dc8313f7f777ed8d264726", { input });
+
+          try {
+            // Fetch the caption zip file
+            Logger.info('Training Route - Fetching caption zip file.');
+            const captionZipBuffer = Buffer.from(await fetch(output).then(res => res.arrayBuffer()));
+            const captionFiles = await extractZip(captionZipBuffer);
+
+            try {
+              // Combine resized images and caption files into a new zip file
+              Logger.info('Training Route - Combining resized images and caption files into a new zip file.');
+              const combinedFiles = [...resizedImages, ...captionFiles];
+              const combinedZipContent = await createZip(combinedFiles);
+
+              try {
+                // Upload the combined zip file to Firebase Storage
+                Logger.info('Training Route - Uploading combined zip file to Firebase Storage.');
+                const combinedDownloadURL = await uploadZip(images[0], combinedZipContent, userUID, userGivenName);
+
+                try { // Create the model
+                  Logger.info('Training Route - Creating the model.');
+                  const owner = 'dalsabrook';
+                  const visibility = 'private';
+                  const hardware = 'gpu-t4';
+                  const description = 'AICurbAppeal.com house model'
+
+                  await replicate.models.create(
+                    owner,
+                    userGivenName,
+                    {
+                      'visibility': visibility,
+                      'hardware': hardware,
+                      'description': description
+                    }
+                  );
+
+                  try { // Training the model that was just created
+                    Logger.info('Training Route - Training the model.');
+                    const modelOwner = 'ostris';
+                    const modelName = 'flux-dev-lora-trainer';
+                    const versionId = '3f39d8b7d50801daf27c7adc4e6b3de138cce9c5daa22e062c5ca30f0a558918';
+
+                    const options = {
+                      destination: `${owner}/${userGivenName}`,
+                      input: {
+                        steps: 1000,
+                        lora_rank: 16,
+                        optimizer: "adamw8bit",
+                        batch_size: 1,
+                        resolution: "512,768,1024",
+                        autocaption: false,
+                        input_images: combinedDownloadURL.zipDownloadURL,
+                        trigger_word: "TOK", // Possibly Set to House?
+                        learning_rate: 0.0004,
+                        wandb_project: "flux_train_replicate",
+                        wandb_save_interval: 100,
+                        caption_dropout_rate: 0.05,
+                        wandb_sample_interval: 100
+                      },
+                      webhook: `https://ai-curb-appeal.vercel.app/api/training-webhook?uid=${userUID}&modelName=${userGivenName}&trainedImg=${combinedDownloadURL.imageDownloadURL}`
+                      // Add query params like user.uid, model name? to then save in db from webhook?
+                    };
+
+                    const training = await replicate.trainings.create(modelOwner, modelName, versionId, options);
+
+                    // Possibly delete the .zip from firebase after use.
+                    // Not sure if it is cheaper to keep the files or use operations to delete them
+
+                    Logger.info(`Training Route - Training URL: https://replicate.com/p/${training.id}`);
+                    return NextResponse.json({ detail: 'Model training has started!', trainedModel: training }, { status: 200 });
+                  } catch (error) {
+                    Logger.error('Training Route - Error during model training:', error);
+                    return NextResponse.json({ detail: 'Error during model training', error: error.message }, { status: 500 });
+                  }
+                } catch (error) {
+                  Logger.error('Training Route - Error creating model:', error);
+                  return NextResponse.json({ detail: 'Error creating the model', error: error.message }, { status: 500 });
+                }
+              } catch (error) {
+                Logger.error('Training Route - Error uploading combined zip file to Firebase Storage:', error);
+                return NextResponse.json({ detail: 'Error uploading combined zip file to Firebase Storage', error: error.message }, { status: 500 });
+              }
+            } catch (error) {
+              Logger.error('Training Route - Error combining resized images and caption files into a new zip file:', error);
+              return NextResponse.json({ detail: 'Error combining resized images and caption files into a new zip file', error: error.message }, { status: 500 });
+            }
+          } catch (error) {
+            Logger.error('Training Route - Error fetching caption zip file:', error);
+            return NextResponse.json({ detail: 'Error fetching caption zip file', error: error.message }, { status: 500 });
+          }
+        } catch (error) {
+          Logger.error('Training Route - Error creating zip file from resized images:', error);
+          return NextResponse.json({ detail: 'Error creating zip file from resized images', error: error.message }, { status: 500 });
+        }
+      } catch (error) {
+        Logger.error('Training Route - Error resizing images:', error);
+        return NextResponse.json({ detail: 'Error resizing images', error: error.message }, { status: 500 });
+      }
     } catch (error) {
-      Logger.error(error);
-      return NextResponse.json({ detail: 'Error during model training', error: error.message }, { status: 500 });
+      Logger.error('Training Route - Error processing form data:', error);
+      return NextResponse.json({ detail: 'Error processing form data', error: error.message }, { status: 500 });
     }
   } catch (error) {
-    Logger.error('Training Route - Error creating model:', error);
-    return NextResponse.json({ detail: 'Error creating the model', error: error.message }, { status: 500 });
+    Logger.error('Training Route - Error handling POST request:', error);
+    return NextResponse.json({ detail: 'Error handling POST request', error: error.message }, { status: 500 });
   }
 }
 
-function GET(req, res) {
+export function GET(req, res) {
   return NextResponse.json('Working!', { status: 200 });
 }
